e8a0603ab03caccf161a2fb68423e1bf453d302f
vishalvaishnav2405 <70995352+vishalvaishnav2405@users.noreply.github.com>
2020-09-14 12:40:10 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a, b, c;
+   
+   cout << "Enter two integers to add\n";
+   cin >> a >> b;
+
+   c = a + b;
+   cout << a <<"+"<< b <<"="<<c ;
+   
+   return 0;
+}
---
+++ print 2 numbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int x;
+   cout <<"Enter the number:\n";
+   cin >> x;
+   cout <<"The number entered by user is " <<x;
+   return 0;
+}
